1411c1c42e94b67ffecd681ce2eafb939c7f4eb5
vinaygahane <56063400+vinaygahane@users.noreply.github.com>
2026-02-04 22:28:25 +0530

    added entity structure to project

---
+++ src/main/java/com/vinayGahane/airBnbApp/entity/enums/BookingStatus.java
@@ -0,0 +1,7 @@
+package com.vinayGahane.airBnbApp.entity.enums;
+
+public enum BookingStatus {
+    RESERVED,
+    CONFIRMED,
+    CANCELLED
+}
---
+++ src/main/java/com/vinayGahane/airBnbApp/entity/enums/Role.java
@@ -0,0 +1,6 @@
+package com.vinayGahane.airBnbApp.entity.enums;
+
+public enum Role {
+    GUEST,
+    HOTEL_MANAGER
+}
